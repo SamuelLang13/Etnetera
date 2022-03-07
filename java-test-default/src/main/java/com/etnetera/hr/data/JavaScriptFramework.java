8ae3637191e6adc670b61c6965b0c0b92597afad
SamuelLang13 <langsamuel10@gmail.com>
2022-03-07 09:14:46 +0100

    Adding properties

--- java-test-default/src/main/java/com/etnetera/hr/data/JavaScriptFramework.java
+++ java-test-default/src/main/java/com/etnetera/hr/data/JavaScriptFramework.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 /**
  * Simple data entity describing basic properties of every JavaScript framework.
@@ -21,6 +22,22 @@ public class JavaScriptFramework {
 
 	@Column(nullable = false, length = 30)
 	private String name;
+
+	@Column(nullable = false, length = 50)
+	private String version;
+
+	@Column(nullable = false)
+	private LocalDate deprecationDate;
+
+	@Column(nullable = false)
+	private Double hypeLevel;
+
+	public JavaScriptFramework(String name, String version, LocalDate deprecationDate, Double hypeLevel) {
+		this.name = name;
+		this.version = version;
+		this.deprecationDate = deprecationDate;
+		this.hypeLevel = hypeLevel;
+	}
 
 	public JavaScriptFramework() {
 	}
@@ -43,6 +60,30 @@ public class JavaScriptFramework {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public LocalDate getDeprecationDate() {
+		return deprecationDate;
+	}
+
+	public void setDeprecationDate(LocalDate deprecationDate) {
+		this.deprecationDate = deprecationDate;
+	}
+
+	public Double getHypeLevel() {
+		return hypeLevel;
+	}
+
+	public void setHypeLevel(Double hypeLevel) {
+		this.hypeLevel = hypeLevel;
 	}
 
 	@Override
