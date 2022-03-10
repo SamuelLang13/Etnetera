b00e600bc662655482a057ebb01b66b0172c3467
SamuelLang13 <langsamuel10@gmail.com>
2022-03-10 12:38:24 +0100

    Test for creating already existing framework

--- java-test-default/src/test/java/com/etnetera/hr/JavaScriptFrameworkControllerTests.java
+++ java-test-default/src/test/java/com/etnetera/hr/JavaScriptFrameworkControllerTests.java
@@ -7,6 +7,7 @@ import com.etnetera.hr.service.JavaScriptFrameworkService;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.stubbing.OngoingStubbing;
@@ -24,9 +25,10 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.AdditionalMatchers.not;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -77,26 +79,53 @@ public class JavaScriptFrameworkControllerTests {
         Mockito.when(service.readById(javaScriptFramework.getId())).thenReturn(javaScriptFramework);
         Mockito.when(service.readById(not(eq(javaScriptFramework.getId())))).thenThrow(new EntityNotFoundException());
 
-        mockMvc.perform(get("/frameworks/-1"))
+        mockMvc.perform(get("/frameworks/getById/-1"))
                 .andExpect(status().isNotFound());
 
+        //Checking to never use delete method if the id does not exist
+        verify(service,never()).delete(any());
 
+        mockMvc.perform(delete("/frameworks/1"))
+                .andExpect(status().isOk());
+
+        //Checking if the correct JSF was deleted
+        verify(service,times(1)).delete(javaScriptFramework.getId());
+    }
+
+    @Test
+    public void testCreateExisting() throws Exception {
+
+        doThrow(new EntityStateException()).when(service).create(any(JavaScriptFramework.class));
+
+        mockMvc.perform(post("/frameworks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"JavaScriptFramework1\"," +
+                                "\"version\":[\"1.3\",\"3.3\"]," +
+                                "\"deprecationDate\":\"2022-03-08\"," +
+                                "\"hypeLevel\":\"43.4\"}"))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void testCreate() throws Exception{
+        JavaScriptFramework javaScriptFramework = new JavaScriptFramework(1L,"JS1", List.of("13.4"), LocalDate.of(2022,2,4),8.7);
+        Mockito.when(service.readById(javaScriptFramework.getId())).thenReturn(javaScriptFramework);
+        Mockito.when(service.readById(not(eq(javaScriptFramework.getId())))).thenThrow(new EntityNotFoundException());
+
+        mockMvc.perform(post("/frameworks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"JS1\"," +
+                                "\"version\":[\"13.4\"]," +
+                                "\"deprecationDate\":\"2022-02-04\"," +
+                                "\"hypeLevel\":\"8.7\"}"))
+                .andExpect(status().isOk());
+
+        ArgumentCaptor<JavaScriptFramework> argumentCaptor = ArgumentCaptor.forClass(JavaScriptFramework.class);
+        Mockito.verify(service, Mockito.times(1)).create(argumentCaptor.capture());
+        JavaScriptFramework javaScriptFramework1 = argumentCaptor.getValue();
+        assertEquals("JS1",javaScriptFramework1.getName());
 
     }
 
-
-//    @Test
-//    public void testCreateExisting() throws Exception {
-//
-//        doThrow(new EntityStateException()).when(service).create(any(JavaScriptFramework.class));
-//
-//        mockMvc.perform(post("/frameworks")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"name\":\"JavaScriptFramework1\"," +
-//                                "\"version\":[\"1.3\",\"3.3\"]," +
-//                                "\"deprecationDate\":\"2022-03-08\"," +
-//                                "\"hypeLevel\":\"43.4\"}"))
-//                .andExpect(status().isInternalServerError());
-//    }
 
 }
