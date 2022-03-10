d9b9c826113bf1f2a45b10088ae4789c540a5a4c
SamuelLang13 <langsamuel10@gmail.com>
2022-03-10 20:25:56 +0100

    Working on service tests

--- java-test-default/src/test/java/com/etnetera/hr/JavaScriptFrameworkServiceTests.java
+++ java-test-default/src/test/java/com/etnetera/hr/JavaScriptFrameworkServiceTests.java
@@ -1,4 +1,49 @@
 package com.etnetera.hr;
 
+import com.etnetera.hr.controller.JavaScriptFrameworkController;
+import com.etnetera.hr.data.JavaScriptFramework;
+import com.etnetera.hr.repository.JavaScriptFrameworkRepository;
+import com.etnetera.hr.service.JavaScriptFrameworkService;
+import org.hibernate.service.spi.InjectService;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.AdditionalMatchers.not;
+import static org.mockito.ArgumentMatchers.*;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class JavaScriptFrameworkServiceTests {
+
+    @Mock
+    JavaScriptFrameworkRepository repository;
+
+    @InjectMocks
+    JavaScriptFrameworkService service;
+
+    @Test
+    public void testExists(){
+        JavaScriptFramework javaScriptFramework1 = new JavaScriptFramework("JS1", List.of("13.4"), LocalDate.of(2022,02,04),8.7);
+        JavaScriptFramework javaScriptFramework2 = new JavaScriptFramework("JS2", List.of("23.4"), LocalDate.of(2019,04,07),5.6);
+
+
+
+    }
+
 }
